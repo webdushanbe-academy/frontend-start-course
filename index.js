56b5d831dc4925be85d5b8e84e0aeffe14e96112
Mr. CaT <mrcat323@gmail.com>
2021-10-01 19:09:36 +0500

    Loops, Arrays

--- index.js
+++ index.js
@@ -1,21 +1,76 @@
 var students = [
-    'Faridun',
-    'Bilol',
-    'Abdullo',
-    'Muhammad',
-    'Muhammad',
-    'Saida',
-    'Intizor',
-    'Bobojon',
-    'Aziz',
-    'Somon',
-    'Qurbonali',
-    'Mehrubon'
+    //  0          1
+    'Muhammad', 'Bilol'
 ];
 
-var a = 5;
-var b = 3;
+let numbers = [];
 
-for (var i = 0; i < 5; ++i) {
-    console.log('' + a);
+
+/*
+console.log(students[0]);
+console.log(students[1]); 
+*/
+/*
+for (var i = 1; i <= 100; ++i) {
+    numbers.push(i);
 }
+*/
+// i = 0;
+// length = 2;
+// i < length = 1;
+
+/*
+i = i + 1;
+i += 1;
+i++
+++i
+*/
+
+/*
+for (var i = 0; i < students.length; ++i) {
+    console.log(students[i]);
+} 
+*/
+
+/*
+for (var iterator = 0; iterator < numbers.length; ++iterator) {
+    console.log('' + numbers[iterator]);
+}
+*/
+
+/*
+for (initialization; condition; increment/decrement/ending) {
+    statements
+}
+*/
+/*
+var i = 0;
+
+while(i < students.length) {
+    console.log(students[i]);
+
+    ++i;
+}
+*/
+
+var students = 'Muhammad,Bilol,Faridun,Abdullah';
+/*
+var reverse = '';
+
+for (var i = name.length - 1; i >= 0; --i) {
+    reverse += name[i];
+}
+
+console.log(reverse);
+
+console.log(name.reverse()); */
+
+var name = 'Faridun';
+
+console.log(name.split('').reverse().join('').toLowerCase());
+
+for (var j = 0; j < 10; ++j) {
+    console.log(name.split('').reverse().join('').toLowerCase());
+}
+
+
